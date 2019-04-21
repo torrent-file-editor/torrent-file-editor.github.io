5039dd63e49b7703a87489427efcd9c5e8f33ef4
Ivan Romanov <drizt72@zoho.eu>
2019-04-21 13:34:15 +0500

    Update

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
 function onloadfunc()
 {
-    let el = document.querySelector('header a.btn');
-    el.setAttribute('tooltip', 'some value');
+    let map = JSON.parse(httpGet('https://api.github.com/repos/torrent-file-editor/torrent-file-editor/releases/latest'));
+
+    if (map) {
+        let el = document.querySelector('header a.btn');
+        el.setAttribute('tooltip', map.url);
+    }
 }
